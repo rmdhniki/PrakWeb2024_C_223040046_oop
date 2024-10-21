6a53daa8215168abac4d37fdcc1a3e9beb575a94
rmdhniki <rmdhniki18@gmail.com>
2024-10-21 17:06:07 +0700

    Vidio 4 - Controller

---
+++ mvc/app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+    public function index($nama = 'Rifki', $pekerjaan = 'Mahasiswa')
+    {
+        echo 'Halo, nama saya $nama, saya adalah $pekerjaan';
+    }
+    public function page ()
+    {
+        echo 'About/page';
+    }
+}
