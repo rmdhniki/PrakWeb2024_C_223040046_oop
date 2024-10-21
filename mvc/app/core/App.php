ae27973b820521676a2fff340b4347103f91b0b3
rmdhniki <rmdhniki18@gmail.com>
2024-10-21 16:11:22 +0700

    Vidio 3 - Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -3,6 +3,16 @@
 class App {
     public function __construct()
     {
-        Echo 'ok!';
+        $url = $this->parseURL();
+    }
+
+    public function parseURL()
+    {
+        if( isset($_GET['url']) ){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
