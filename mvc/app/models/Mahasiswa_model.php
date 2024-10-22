5cfd46abc94b718e2d3506840afef91f56e10809
rmdhniki <rmdhniki18@gmail.com>
2024-10-23 02:38:04 +0700

    Vidio 9 - Insert Data

--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -1,22 +1,34 @@
 <?php 
+class Mahasiswa_model {
+    private $table = 'mahasiswa';
+    private $db;
 
-class Mahasiswa_model{
+    public function __construct() {
+        $this->db = new Database;
+    }
 
-  private $table = 'mahasiswa';
-  private $db;
-  public function __construct()
-  {
-    $this->db = new Database;
-  }
-  public function getAllMahasiswa () {
-    $this->db->query(' SELECT * FROM ' . $this->table );
-    return $this->db->resultSet();
-  }
+    public function getAllMahasiswa () {
+       $this->db->query(' SELECT * FROM ' . $this->table );
+       return $this->db->resultSet();
+    }
+
     public function getMahasiswaById ($id) {
         $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id = :id');
 
         $this->db->bind('id', $id);
         return $this->db->single();
+    }
+    public function tambahDataMahasiswa($data) {
+        $query = "INSERT INTO mahasiswa (nama, nrp, email, jurusan)
+                    VALUES
+                    ( :nama, :nrp, :email, :jurusan)";
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+        $this->db->execute();
+        return $this->db->rowCount();
     }
 }
 ?>
